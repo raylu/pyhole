b52cac78c02f1405d6b76bebe7b52822e804fe9b
raylu <ray.lu@getclever.com>
2013-05-24 01:49:20 -0700

    bootstrap

---
+++ config.py
@@ -0,0 +1,20 @@
+import yaml
+
+class Config(object):
+	def __init__(self, cdict):
+		attrs = set(self.attrs) # copy and "unfreeze"
+		for k, v in cdict.items():
+			attrs.remove(k) # check if the key is allowed, mark it as present
+			setattr(self, k, v)
+		if len(attrs) != 0:
+			raise KeyError('missing required bot config keys: %s' % attrs)
+
+class WebConfig(Config):
+	attrs = frozenset([
+		'port',
+		'host',
+		'cookie_secret',
+	])
+
+__doc = yaml.load(open('config.yaml', 'r'))
+web = WebConfig(__doc['web'])
---
+++ server.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import json
+from lesscss import lessc
+import tornado.gen
+import tornado.httpclient
+import tornado.ioloop
+import tornado.web
+import operator
+import os
+
+import config
+
+class BaseHandler(tornado.web.RequestHandler):
+	def render(self, *args, **kwargs):
+		kwargs['host'] = config.web.host
+		return super(BaseHandler, self).render(*args, **kwargs)
+
+	def render_string(self, *args, **kwargs):
+		s = super(BaseHandler, self).render_string(*args, **kwargs)
+		return s.replace(b'\n', b'') # this is like Django's {% spaceless %}
+
+class MainHandler(BaseHandler):
+	@tornado.web.asynchronous
+	@tornado.gen.coroutine
+	def get(self):
+		self.render('home.html')
+
+
+class CSSHandler(tornado.web.RequestHandler):
+	def get(self, css_path):
+		css_path = os.path.join(os.path.dirname(__file__), 'static', css_path) + '.less'
+		with open(css_path, 'r') as f:
+			self.set_header('Content-Type', 'text/css')
+			css = lessc.compile(f.read())
+			self.write(css)
+
+if __name__ == '__main__':
+	tornado.web.Application(
+		handlers=[
+			(r'/', MainHandler),
+			(r"/(css/.+)\.css", CSSHandler),
+		],
+		template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+		static_path=os.path.join(os.path.dirname(__file__), 'static'),
+		cookie_secret=config.web.cookie_secret,
+		debug=True,
+	).listen(config.web.port)
+	print('Listening on :%d' % config.web.port)
+	tornado.ioloop.IOLoop.instance().start()
