6cf6c12604084b03e158039f2d52031b4cf1f934
raylu <ray.lu@getclever.com>
2013-05-25 02:22:51 -0700

    xsrf, current_user

--- server.py
+++ server.py
@@ -19,6 +19,11 @@ class BaseHandler(tornado.web.RequestHandler):
 		s = super(BaseHandler, self).render_string(*args, **kwargs)
 		return s.replace(b'\n', b'') # this is like Django's {% spaceless %}
 
+	def get_current_user(self):
+		user_id = self.get_secure_cookie('user_id')
+		if user_id is not None:
+			return int(user_id)
+
 class MainHandler(BaseHandler):
 	def get(self):
 		self.render('home.html')
@@ -27,8 +32,12 @@ class LoginHandler(BaseHandler):
 	def post(self):
 		username = self.get_argument('username')
 		password = self.get_argument('password')
-		status = db.check_login(username, password)
-		self.render('login.html', status=status)
+		user_id = db.check_login(username, password)
+		success = False
+		if user_id is not None:
+			self.set_secure_cookie('user_id', str(user_id), expires_days=90)
+			success = True
+		self.render('login.html', success=success)
 
 class CSSHandler(tornado.web.RequestHandler):
 	def get(self, css_path):
@@ -48,6 +57,7 @@ if __name__ == '__main__':
 		template_path=os.path.join(os.path.dirname(__file__), 'templates'),
 		static_path=os.path.join(os.path.dirname(__file__), 'static'),
 		cookie_secret=config.web.cookie_secret,
+		xsrf_cookies=True,
 		debug=True,
 	).listen(config.web.port)
 	print('Listening on :%d' % config.web.port)
