raylu/pyhole

commit 1ad177f5c61994e5aeeb9a3e92a078d4bad9c7b2

refactor db.py

db.py
@@ -7,20 +7,19 @@ import os
 
 conn = oursql.connect(db='pyhole', user='pyhole', passwd='pyhole', autoreconnect=True)
 
-def query(sql, *args):
-	with conn.cursor() as c:
-		c.execute(sql, args)
-		while True:
-			r = c.fetchone()
-			if r is None:
-				break
-			attribs = DBRow()
-			for i, f in enumerate(c.description):
-				setattr(attribs, f[0], r[i])
-			yield attribs
+def query(cursor, sql, *args):
+	cursor.execute(sql, args)
+	while True:
+		r = cursor.fetchone()
+		if r is None:
+			break
+		attribs = DBRow()
+		for i, f in enumerate(cursor.description):
+			setattr(attribs, f[0], r[i])
+		yield attribs
 
-def query_one(sql, *args):
-	results = query(sql, *args)
+def query_one(cursor, sql, *args):
+	results = query(cursor, sql, *args)
 	try:
 		r = next(results)
 	except StopIteration:
@@ -32,21 +31,18 @@ def query_one(sql, *args):
 	else:
 		raise RuntimeError('multiple rows for query {}, {}'.format(sql, args))
 
-def execute(sql, *args):
-	with conn.cursor() as c:
-		c.execute(sql, args)
-		return c.lastrowid
-
 def create_user(username, password):
 	salt = os.urandom(16)
 	h = hmac.new(salt, password.encode('utf-8'), hashlib.sha256)
 	hashed = h.hexdigest()
 	salt_hex = binascii.hexlify(salt)
-	execute('INSERT INTO users (username, password, salt) VALUES(?, ?, ?)',
-	        username, hashed, salt_hex)
+	with conn.cursor() as c:
+		c.execute('INSERT INTO users (username, password, salt) VALUES(?, ?, ?)',
+				username, hashed, salt_hex)
 
 def check_login(username, password):
-	r = query_one('SELECT id, password, salt FROM users WHERE username = ?', username)
+	with conn.cursor() as c:
+		r = query_one(c, 'SELECT id, password, salt FROM users WHERE username = ?', username)
 	if r is None:
 		return
 	salt = binascii.unhexlify(bytes(r.salt, 'ascii'))
@@ -58,8 +54,8 @@ class UpdateError(Exception):
 	def __init__(self, message):
 		self.message = message
 
-def update_map(system):
-	def update_node(node):
+def add_system(system):
+	def add_node(node):
 		if node['name'] == system['src']:
 			node.setdefault('connections', [])
 			o = {'name': system['dest']}
@@ -73,16 +69,17 @@ def update_map(system):
 			return True
 		if 'connections' in node:
 			for c in node['connections']:
-				if update_node(c):
+				if add_node(c):
 					return True
 
 	with conn.cursor() as c:
-		c.execute('SELECT json from maps')
-		r = c.fetchone()
-		map_data = json.loads(r[0])
-		if not update_node(map_data):
+		r = query_one(c, 'SELECT json from maps')
+		map_data = json.loads(r.json)
+		if not add_node(map_data):
 			raise UpdateError('src system not found')
-		c.execute('UPDATE maps SET json = ?', (json.dumps(map_data),))
+		map_json = json.dumps(map_data)
+		c.execute('UPDATE maps SET json = ?', (map_json,))
+	return map_json
 
 def delete_system(system_name):
 	def delete_node(node):
@@ -95,14 +92,15 @@ def delete_system(system_name):
 					return True
 
 	with conn.cursor() as c:
-		c.execute('SELECT json from maps')
-		r = c.fetchone()
-		map_data = json.loads(r[0])
+		r = query_one(c, 'SELECT json from maps')
+		map_data = json.loads(r.json)
 		if map_data['name'] == system_name:
 			raise UpdateError('cannot delete root node')
 		if not delete_node(map_data): # this will not delete the root node (even if it passed previous check)
 			raise UpdateError('system not found')
-		c.execute('UPDATE maps SET json = ?', (json.dumps(map_data),))
+		map_json = json.dumps(map_data)
+		c.execute('UPDATE maps SET json = ?', (map_json,))
+	return map_json
 
 class DBRow:
 	def __str__(self):

server.py
@@ -58,10 +58,8 @@ class MapWSHandler(tornado.websocket.WebSocketHandler):
 		else:
 			print('unhandled message', message)
 
-	def __send_map(self):
-		r = db.query_one('SELECT json from maps')
-		map_data = r.json
-		self.write_message('MAP ' + map_data)
+	def __send_map(self, map_json):
+		self.write_message('MAP ' + map_json)
 
 	def __send_err(self, e):
 		self.write_message('ERR ' + e.message)
@@ -74,20 +72,23 @@ class MapWSHandler(tornado.websocket.WebSocketHandler):
 			user_id = int(tornado.web.decode_signed_value(config.web.cookie_secret, "user_id", user_id_cookie))
 		except KeyError:
 			return
-		self.__send_map()
+		with db.conn.cursor() as c:
+			r = db.query_one(c, 'SELECT json from maps')
+		map_json = r.json
+		self.__send_map(map_json)
 
 	def add(self, system_json):
-		system = json.loads(system_json)
 		try:
-			db.update_map(system)
-			self.__send_map()
+			system = json.loads(system_json)
+			map_json = db.add_system(system)
+			self.__send_map(map_json)
 		except db.UpdateError as e:
 			self.__send_err(e)
 
 	def delete(self, system_name):
 		try:
-			db.delete_system(system_name)
-			self.__send_map()
+			map_json = db.delete_system(system_name)
+			self.__send_map(map_json)
 		except db.UpdateError as e:
 			self.__send_err(e)
 

static/js/map.js
@@ -41,6 +41,11 @@ window.addEvent('domready', function() {
 		modal('ruh roh!');
 		ws.close();
 	}
+	function send(command, args) {
+		var message = command + ' ' + args;
+		console.debug(message);
+		ws.send(message);
+	}
 
 	function drawNode(node, x, y) {
 		drawSystem(node, x, y);
@@ -105,9 +110,7 @@ window.addEvent('domready', function() {
 		bottom_divs.setStyle('display', 'block');
 	}
 	$('delete').addEvent('click', function(e) {
-		var sn_str = system_name.get('text');
-		console.debug('DELETE', sn_str);
-		ws.send('DELETE ' + sn_str);
+		send('DELETE', system_name.get('text'));
 		bottom_divs.setStyle('display', 'none');
 	});
 	var add_form = $('add');
@@ -131,8 +134,7 @@ window.addEvent('domready', function() {
 			modal("you didn't specify a system name");
 			return;
 		}
-		console.debug('ADD', o);
-		ws.send('ADD ' + JSON.stringify(o));
+		send('ADD', JSON.stringify(o));
 
 		['dest', 'to', 'from'].each(function(id) {
 			$(id).set('value', '');

templates/base.html
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PyHole</title>
+	<title>pyhole</title>
 	<link rel="stylesheet" type="text/css" href="/css/base.css" />
 	<script src="//ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools.js"></script>
 	{% block js %}{% end %}

commit f8f50c8c6adeb429c17d42ccea3924503b2e352c

fix create_user
broken in 1ad177f

db.py
@@ -39,7 +39,7 @@ def create_user(username, password):
 	salt_hex = binascii.hexlify(salt)
 	with conn.cursor() as c:
 		c.execute('INSERT INTO users (username, password, salt) VALUES(?, ?, ?)',
-				username, hashed, salt_hex)
+				[username, hashed, salt_hex])
 
 def check_login(username, password):
 	with conn.cursor() as c: